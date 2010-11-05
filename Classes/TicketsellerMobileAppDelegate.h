75acf15a54a05d3ab1919e8b3d405b7f93e56f88
Craig Plummer <craigplummer@me.com>
2010-11-05 20:09:45 +0000

    Inital Commit of iPhone Application

---
+++ Classes/TicketsellerMobileAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  TicketsellerMobileAppDelegate.h
+//  TicketsellerMobile
+//
+//  Created by Craig on 30/10/2010.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface TicketsellerMobileAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
