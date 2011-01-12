083f386e966aefb98f895159b5c2c0ab5f0f5d21
Craig Plummer <craigplummer@me.com>
2011-01-12 23:40:42 +0000

    Updated ticketseller application - with working Login / Logout
    Signed-off-by: Craig Plummer <craigplummer@me.com>

---
+++ ticketseller-mobile/Classes/ticketseller_mobileAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  ticketseller_mobileAppDelegate.h
+//  ticketseller-mobile
+//
+//  Created by Craig Plummer on 12/01/2011.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface ticketseller_mobileAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ ticketseller-mobile/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ticketseller-mobile
+//
+//  Created by Craig Plummer on 12/01/2011.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"ticketseller_mobileAppDelegate");
+    [pool release];
+    return retVal;
+}
